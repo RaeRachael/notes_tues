d8211a73e30764f3fd7fc22e9570f669a444dbdb
Rachael <rachaelewins@hotmail.com>
2020-06-24 12:10:06 +0100

    notController works, not tested

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,13 @@
-var why = document.getElementById("app").innerHTML
-console.log(why)
-document.getElementById("app").innerHTML = "now i work!! "
+function NoteListController(list) {
+  this.list = list;
+  list.addNote("Favourite drink: seltzer");
+  this.listView = new NoteListView(list);
+};
+
+NoteListController.prototype.addHTML = function() {
+  document.getElementById("app").innerHTML = this.listView.returnHTML();
+};
+
+var noteListController = new NoteListController(new NoteList);
+console.log(noteListController);
+noteListController.addHTML()
